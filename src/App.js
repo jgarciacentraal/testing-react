304c6de80850a78755000ef705716a4961920491
Julio Garcia Ramos <juliogarciaramos@MacBook-Pro-de-Julio.local>
2023-02-12 18:38:24 -0600

    some assertions and fire events

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import * as React from 'react'
 import './App.css';
 
 function App() {
+  const [state, setState] = React.useState(true)
+  const [checkboxState, setCheckboxState] = React.useState(false)
+  const [ableContainer, setAbleContainer] = React.useState(false)
+
+
+  const handleState = () => setState(!state)
+
+  const getText = state => state ? "Click me!" : "I was clicked!!"
+
+  
+
+  const handleClickCheckbox = () => {
+    setCheckboxState(!checkboxState)
+    setAbleContainer(!ableContainer)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        onClick={handleState} 
+        style={{backgroundColor: 'red', borderStyle: 'none'}}>{getText(state)}
+      </button>
+      <form>
+        <label htmlFor='checkbox-input'>Please accept terms</label>
+        <input id='checkbox-input' type="checkbox" onClick={handleClickCheckbox} value={checkboxState} />
+      </form>
+      {ableContainer ? <div data-testid='test-container'>Im here</div> : null}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,35 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders a red button', () => {
+  render(<App />)
+  const button = screen.getByRole("button", { name: "Click me!" })
+    expect(button).toHaveStyle({backgroundColor:"red"})
 });
+
+test('when clicks button change to blue', () => { 
+  render(<App />)
+  const button = screen.getByRole("button", { name: "Click me!" })  
+  fireEvent.click(button)
+  expect(button).toHaveTextContent('I was clicked!!')
+})
+ 
+test('should display checkbox', async () => {
+  render(<App />)
+  const label = screen.getByLabelText('Please accept terms')
+  const checkbox = screen.getByRole('checkbox')  
+  expect(checkbox).toBeInTheDocument()
+  expect(label).toBeInTheDocument()
+})
+
+test('should display container after click checkbox', () => { 
+  render(<App />)
+  //show html element
+  const checkbox = screen.getByRole('checkbox') 
+  //trigger click fn 
+  fireEvent.click(checkbox)
+  //enables container changing value for conditional
+  const container = screen.getByTestId('test-container')
+  //assert container
+  expect(container).toBeInTheDocument()
+
+ })
